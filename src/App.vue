04ed65d7389293a1ce26c2918ccd365aadd26adc
Kevin McCarthy <mccarthyk@hcflgov.net>
2017-07-18 16:59:48 -0400

    init main app functionality

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+	<div id="app" class="embed-responsive embed-responsive-16by9">
+		<div id="mapDiv" class="embed-responsive-item"></div>
+		<!-- <router-view></router-view> -->
+	</div>
 </template>
 
 <script>
+import * as esriLoader from 'esri-loader'
+
 export default {
-  name: 'app'
+	name: 'app',
+	props: ['portalId'],
+	data () {
+		return {
+			webmap: null,
+			mapview: null
+		}
+	},
+	mounted () {
+
+		esriLoader.dojoRequire([
+			'esri/WebMap',
+			'esri/views/MapView',
+			'esri/widgets/Home'
+		], (WebMap, MapView, Home) => {
+
+			this.webmap = new WebMap({
+				portalItem: {
+					id: this.portalId || 'b51fb4e76e154e1b93b630eac3ea94ae'
+				}
+			})
+
+			this.mapview = new MapView({
+				container: 'mapDiv',
+				map: this.webmap
+			})
+
+			var homeWidget = new Home({
+				view: this.mapview
+			})
+
+			// adds the home widget to the top left corner of the MapView
+			this.mapview.ui.add(homeWidget, "top-left")
+
+		});
+
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
